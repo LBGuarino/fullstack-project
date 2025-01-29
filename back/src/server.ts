d81c6075e555d321547d7691f401ee45a5ab5483
lbguarino <lbelen2014@gmail.com>
2025-01-29 14:57:56 +0000

    final net

--- back/src/server.ts
+++ back/src/server.ts
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://www.thescentedshop.blog",
   credentials: true,
 }));
 app.use(express.json());
