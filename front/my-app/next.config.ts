f4461263b18074daac3df87c68082d46475b63c9
lbguarino <lbelen2014@gmail.com>
2025-01-30 18:51:24 +0000

    next config

--- front/my-app/next.config.ts
+++ front/my-app/next.config.ts
@@ -14,6 +14,19 @@ const nextConfig: NextConfig = {
         destination: 'https://fullstack-project-back-mtag.onrender.com/:path*'
       }
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://thescentedshop.blog" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization" }
+        ]
+      }
+    ];
   }
 };
 
