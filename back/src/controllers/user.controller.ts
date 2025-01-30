87647eaf9f3934c3d8b2367bd1e05f0aafd2cc33
lbguarino <lbelen2014@gmail.com>
2025-01-30 10:44:35 +0000

    cookies x3

--- back/src/controllers/user.controller.ts
+++ back/src/controllers/user.controller.ts
@@ -42,7 +42,7 @@ export const login = catchedController(async (req: Request, res: Response) => {
     sameSite: isProduction ? 'none' : 'lax',
     maxAge: 1000 * 60 * 60 * 24 * 7,
     path: '/',
-    domain: isProduction ? '.thescentedshop.blog' : undefined,
+    domain: isProduction ? 'thescentedshop.blog' : undefined,
   })
   res.status(200).send({
     login: true,
@@ -67,7 +67,7 @@ export const getSession = catchedController(async (req: Request, res: Response) 
 
     if (!user) {
       res.clearCookie('token', {
-        domain: '.thescentedshop.blog',
+        domain: 'thescentedshop.blog',
         path: '/'
       });
       return res.status(401).json({ message: "User not found" });
@@ -79,13 +79,13 @@ export const getSession = catchedController(async (req: Request, res: Response) 
       sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24 * 7,
       path: '/',
-      domain: isProduction ? '.thescentedshop.blog' : undefined
+      domain: isProduction ? 'thescentedshop.blog' : undefined
     });
 
     return res.status(200).json({ user });
   } catch (error) {
     res.clearCookie('token', {
-      domain: '.thescentedshop.blog',
+      domain: 'thescentedshop.blog',
       path: '/'
     });
     return res.status(401).json({ message: "Invalid or expired session" });
