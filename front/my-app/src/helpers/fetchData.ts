d403e674ca93a5d6061aeb8497be8bbc178df562
lbguarino <lbelen2014@gmail.com>
2025-01-30 18:48:22 +0000

    cookies solved

--- front/my-app/src/helpers/fetchData.ts
+++ front/my-app/src/helpers/fetchData.ts
@@ -48,10 +48,7 @@ export async function fetchProductsData({ params }: { params: { category: string
   const { category } = await params;
 
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/categories/${category}`, {
-      method: "GET",
-      credentials: "include",
-    });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/categories/${category}`);
     if (!response.ok) {
       throw new Error(`Failed to fetch products for category: ${category}`);
     }
