2afabda4564d790eb54e7ac7db13b0c6cd6e022b
lbguarino <lbelen2014@gmail.com>
2025-01-30 19:17:50 +0000

    maybeee

--- front/my-app/next.config.ts
+++ front/my-app/next.config.ts
@@ -19,19 +19,6 @@ const nextConfig: NextConfig = {
       }
     ]
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://thescentedshop.blog" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization" }
-        ]
-      }
-    ];
-  }
 };
 
 export default nextConfig;
