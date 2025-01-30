be9ab5911d74f8b72f44e5a50152de4bbb10c9c3
lbguarino <lbelen2014@gmail.com>
2025-01-30 22:35:57 +0000

    solved cookie

--- front/my-app/src/helpers/getProduct.ts
+++ front/my-app/src/helpers/getProduct.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getProduct(id: number) {
     try {
-        const product = await axios.get(`api/products/${id}`);
+        const product = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/products/${id}`);
         return product.data;
 
     } catch (error) {
