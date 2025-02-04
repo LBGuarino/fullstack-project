0e4c7946cbc9c486a874b0292e3eb446c935a105
lbguarino <lbelen2014@gmail.com>
2025-02-04 09:53:35 +0000

    hedaers

--- front/my-app/next.config.ts
+++ front/my-app/next.config.ts
@@ -68,7 +68,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: 'api/:path*',
+        source: '/:path*',
         headers: [
           {
             key: 'X-Content-Type-Options',
